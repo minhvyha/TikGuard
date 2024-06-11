2bb4cd097acba35e06bd5980eb353c3b6a1d9b10
Minh Vy ha <minhvy.ha@outlook.com>
2024-06-11 13:08:50 +1000

    chore: Remove search functionality from Main component

---
+++ tikguard/app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound: React.FC = () => {
+  return (
+
+    <div className='fixed h-dvh w-dvw flex  flex-col justify-center items-center'>
+      <div className='text-2xl'>404 - Page Not Found</div>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
--- tikguard/components/Main.tsx
+++ tikguard/components/Main.tsx
@@ -11,16 +11,7 @@ const Main = () => {
   }
   return (
     <div className='flex justify-center flex-col items-center w-full py-8 gap-2 '>
-      <Link href='/' className='text-center flex flex-col gap-2'>
-        <h1 className='text-3xl'>TikGuard</h1>
-      </Link>
-      <div className="">
-        <input type="text" value={searchTerm} onChange={(e) =>{
-          setSearchTerm(e.target.value)}} placeholder="Search..." />
-        <button onClick={search}>
-          <i className="fa fa-search"></i>
-        </button>
-      </div>
+      
     </div>
   )
 }
