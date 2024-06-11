44d3acc3e08d62398b2f982171fa3b6a0ad95948
Minh Vy ha <minhvy.ha@outlook.com>
2024-06-11 12:58:05 +1000

    chore: Update navigation component and logo styling

--- tikguard/components/Nav.tsx
+++ tikguard/components/Nav.tsx
@@ -9,10 +9,10 @@ const Nav = () => {
   const { account } = useContext(MainContext);
 
   return (
-    <nav className="flex items-center justify-between py-4 px-8   border-b-2 border-gray-600 ">
-      <Link href='/' className="flex items-center cursor-pointer">
+    <nav className="flex items-center justify-between py-3 px-8   border-b-2 border-gray-600 ">
+      <Link href='/' className="flex gap-1 items-center justify-center cursor-pointer">
         <Image src={logo} alt='' width={50} height={50} />
-        <h1 className="ml-2">TikGuard</h1>
+        <h1>TikGuard</h1>
       </Link>
       <Link href="https://github.com/minhvyha/TikGuard" className={`px-4 py-2 rounded bg-background-400 text-white hover:bg-background-500 transition-all`}>
         GitHub Repo
