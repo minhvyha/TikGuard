b0394841a912c70e3135735be38765dae636b410
Minh Vy ha <minhvy.ha@outlook.com>
2024-06-14 16:28:12 +1000

    chore: Refactor Main component and update imports

--- tikguard/app/page.tsx
+++ tikguard/app/page.tsx
@@ -1,13 +1,6 @@
 'use client'
 import Image from 'next/image';
-import { createContext } from 'react';
 import Main from '@/components/Main';
-interface MainContextType {
-  account: boolean
-}
-
-export const MainContext = createContext<MainContextType>({
-account: true});
 
 export default function Home() {
   return (
