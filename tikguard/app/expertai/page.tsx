2e63fb023aa3b2ba61c20afdcf532dcb04ca9558
Minh Vy ha <minhvy.ha@outlook.com>
2024-06-14 22:17:41 +1000

    chore: Remove commented out code and unused imports in page and Main components

--- tikguard/app/expertai/page.tsx
+++ tikguard/app/expertai/page.tsx
@@ -13,12 +13,12 @@ var text = "why are you so dumb, you can't even reach the top shelf.";
 const page = () => {
   // console.log(process.env.EAI_USERNAME)
   // console.log(process.env.EAI_PASSWORD)
-  nlClient.detect(text, {
-    language: Language.EN,
-    detector: "hate-speech",
-  }).then((result) => {
-    console.log(result.data?.categories)
-  })
+  // nlClient.detect(text, {
+  //   language: Language.EN,
+  //   detector: "hate-speech",
+  // }).then((result) => {
+  //   console.log(result.data?.categories)
+  // })
   return (
     <div>page</div>
   )
--- tikguard/components/Main.tsx
+++ tikguard/components/Main.tsx
@@ -2,18 +2,27 @@
 
 import React, { useState } from 'react'
 import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+// import { useRouter } from 'next/navigation'
+import { apiPage } from '@/constant'
 import DemoCard from './DemoCard'
 
 const Main = () => {
-  const router = useRouter()
-  const [searchTerm, setSearchTerm] = useState('')
-  function search(){
-    router.push(`/recipe?search=${searchTerm}`)
-  }
+  // const router = useRouter()
+  // const [searchTerm, setSearchTerm] = useState('')
+  // function search(){
+  //   router.push(`/recipe?search=${searchTerm}`)
+  // }
+  let DemoCards = apiPage.map((api, index) => {
+    return (
+      <Link key={index} href={` ${api.path}`}>
+        {api.name}
+      </Link>
+    )
+  })
   return (
     <div className='flex justify-center flex-col items-center w-full py-8 gap-2 '>
-<DemoCard />
+      <DemoCard />
+      {DemoCards}
     </div>
   )
 }
