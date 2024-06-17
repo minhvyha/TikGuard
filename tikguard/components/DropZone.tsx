5db6e205a55eb4f543609f7680ef561da95751d4
Minh Vy ha <minhvy.ha@outlook.com>
2024-06-17 20:45:17 +1000

    Refactor DropZone component, update drag and drop message

--- tikguard/components/DropZone.tsx
+++ tikguard/components/DropZone.tsx
@@ -13,7 +13,7 @@ const DropZone = () => {
     {isDragActive ? (
       <p>Drop the files here ...</p>
     ) : (
-      <p>Drag 'n' drop some files here, or click to select files</p>
+      <p>Drag & drop some files here, or click to select files</p>
     )}
   </div>
   )
