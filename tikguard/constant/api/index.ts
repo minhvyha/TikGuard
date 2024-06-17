6cbe54c16872221d1966601cce14d958f97a2e8f
Minh Vy ha <minhvy.ha@outlook.com>
2024-06-17 17:11:11 +1000

    Refactor API page, update API list with descriptions

--- tikguard/constant/api/index.ts
+++ tikguard/constant/api/index.ts
@@ -3,16 +3,19 @@ let apiPage = [{
   name:"HateHoundAPI",
   url: "https://hate-hound-api.vercel.app/",
   path: "hatehoundapi",
+  description: "AI-powered API for detecting toxic comments swiftly and accurately.",
 },
 {
   name: "expert.ai",
   url: "https://developer.expert.ai",
   path: "expertai",
+  description: "Advanced natural language API for understanding and analyzing text.",
 },
 {
   name: "Fuk.ai",
   url: "https://fuk.ai",
   path: "fukai",
+  description: "Free AI-powered hate speech and profanity detection tool for content moderation."
 }];
 
 export {apiPage}
