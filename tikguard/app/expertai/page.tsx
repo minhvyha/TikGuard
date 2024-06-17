57f82c680dc3222f6b6b3ea8191eca0cab711743
Minh Vy ha <minhvy.ha@outlook.com>
2024-06-17 20:43:26 +1000

    chore: Refactor page component, update imports and remove unused code

--- tikguard/app/expertai/page.tsx
+++ tikguard/app/expertai/page.tsx
@@ -1,18 +1,13 @@
 'use client';
-import React, { useCallback } from 'react';
+import React from 'react';
 import { NLClient } from '@expertai/nlapi';
 import { Language } from '@expertai/nlapi';
-import { useDropzone } from 'react-dropzone';
-
+import DropZone from '@/components/DropZone';
 var nlClient = new NLClient();
 
 var text = "why are you so dumb, you can't even reach the top shelf.";
 
 const page = () => {
-  const onDrop = useCallback((acceptedFiles: File[]) => {
-    // Do something with the files
-  }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   // console.log(process.env.EAI_USERNAME)
   // console.log(process.env.EAI_PASSWORD)
@@ -24,14 +19,7 @@ const page = () => {
   // })
   return (
     <div>
-      <div {...getRootProps()}>
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
-          <p>Drag 'n' drop some files here, or click to select files</p>
-        )}
-      </div>
+      <DropZone />
     </div>
   );
 };
