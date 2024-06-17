c7537577fb89f4a12dacc58234ba6f65bc83200f
Minh Vy ha <minhvy.ha@outlook.com>
2024-06-17 17:20:05 +1000

    Refactor DemoCard component, update imports and descriptions

--- tikguard/components/DemoCard.tsx
+++ tikguard/components/DemoCard.tsx
@@ -6,33 +6,38 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Link from 'next/link';
 
-
-const DemoCard = ({title, path, description, url}: {title: string, path: string, description: string, url: string}) => {
+const DemoCard = ({
+  title,
+  path,
+  description,
+  url,
+}: {
+  title: string;
+  path: string;
+  description: string;
+  url: string;
+}) => {
   return (
     <Card sx={{ bgcolor: 'background.default' }} variant="outlined">
       <CardContent>
-      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-        <Link href={url} target='_blank'>
-        {url}
-        
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          <Link href={url} target="_blank">
+            {url}
+          </Link>
+        </Typography>
+        <Typography variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">{description}</Typography>
+      </CardContent>
+      <CardActions>
+        <Link href={path}>
+          <Button size="small">Try Demo</Button>
         </Link>
-      </Typography>
-      <Typography variant="h5" component="div">
-        {title}
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        adjective
-      </Typography>
-      <Typography variant="body2">
-        {description}
-      </Typography>
-    </CardContent>
-    <CardActions>
-    <Link href={path}>
-      <Button size="small">Try Demo</Button>
-
-      </Link>
-    </CardActions>
+      </CardActions>
     </Card>
   );
 };
