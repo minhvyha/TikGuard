cef130817c029666d98cac098a96324b47a283b3
XavierVTan <132450443+XavierVTan@users.noreply.github.com>
2024-06-20 20:08:19 +1000

    Added some style changes to the overall site.

--- tikguard/components/Nav.tsx
+++ tikguard/components/Nav.tsx
@@ -3,25 +3,26 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useThemeContext } from '../app/context/context'; // Adjust the import path as needed
-import logo from '@/assets/logo.png'; // Adjust the import path as needed
+import { useThemeContext } from '../app/context/context';
+import logo from '@/assets/logo.png'; 
 
 const Nav = () => {
   const { toggleTheme } = useThemeContext();
 
   return (
-    <nav className="flex items-center justify-between py-3 px-8 border-b-2 border-gray-600">
-      <Link href='/' className="flex gap-1 items-center justify-center cursor-pointer">
+    <nav className="bg-gray-800 text-white flex items-center justify-between py-4 px-8 border-b border-gray-700">
+      <Link href='/' className="flex items-center gap-2 cursor-pointer">
         <Image src={logo} alt='logo' width={50} height={50} />
-        <h1>TikGuard</h1>
+        <h1 className="text-2xl font-bold">TikGuard</h1>
       </Link>
 
-      <div className="flex items-center gap-4">
-        <Link href="https://github.com/minhvyha/TikGuard" className="px-4 py-2 rounded bg-background-400 text-white hover:bg-background-500 transition-all">
+      <div className="flex items-center gap-6">
+        <Link href="https://github.com/minhvyha/TikGuard" className="px-4 py-2 rounded bg-gray-600 text-white hover:bg-gray-700 transition-colors duration-300">
           GitHub Repo
         </Link>
         <button
           onClick={toggleTheme}
+          className="px-4 py-2 rounded bg-indigo-600 text-white hover:bg-indigo-700 transition-colors duration-300"
         >
           Toggle Theme
         </button>
