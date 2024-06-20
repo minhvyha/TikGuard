0c713272553699737a070687056bf01495a33c06
Minh Vy ha <minhvy.ha@outlook.com>
2024-06-20 21:59:26 +1000

    chore: Refactor TikGuard database API configuration and remove unnecessary code in index.ts

--- tikguard_database/api/index.ts
+++ tikguard_database/api/index.ts
@@ -1,10 +1,25 @@
 require('dotenv').config();
+import { ReportModel } from "../models/ReportModel";
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-import { ReportModel } from "../models/ReportModel";
-app.get("/", (req, res) => res.send("Express on Vercel"));
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.get("/", (req, res) => res.send("Express on Vercel"));
+connectDB().then(() => {
+  app.listen(3000, () => {
+    console.log('listening for requests on Port: ' + 3000);
+  });
+});
+
 
 module.exports = app;
