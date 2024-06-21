5a4c435023c7c60e82d3c407a2bd680ed00088ca
Minh Vy ha <minhvy.ha@outlook.com>
2024-06-21 13:49:09 +1000

    chore: Add CORS headers to API endpoints

--- tikguard_database/api/index.ts
+++ tikguard_database/api/index.ts
@@ -10,6 +10,18 @@ const { randomUUID } = require('crypto');
 
 
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,OPTIONS,POST,PUT,DELETE'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  next();
+});
 app.use(express.urlencoded({ extended: true }));
 mongoose.set('strictQuery', false);
 const connectDB = async () => {
