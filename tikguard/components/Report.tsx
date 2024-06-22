591b9f692a0470dc8ec4418f995ed0313c87c93e
XavierVTan <132450443+XavierVTan@users.noreply.github.com>
2024-06-22 13:36:59 +1000

    Changed the layout of the Report Component

--- tikguard/components/Report.tsx
+++ tikguard/components/Report.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
 
 const Report = () => {
   return (
-    <div className='bg-background-800 h-full rounded-[3px]'>Report</div>
-  )
+    
+    <div className='bg-background-800 h-full rounded-[3px] p-6 flex flex-col justify-center items-center'>
+      <h1 className='text-2xl text-text-100 mb-4'>Report</h1>
+      <div className='grid grid-cols-2 gap-4' style={{ width: '500px', height: '400px' }}>
+        <div className='flex flex-col items-center justify-center w-full h-full border p-4 rounded-md'>
+          <span className='text-text-100'>Folder Name:</span>
+          <span className='text-text-150'>None</span>
+        </div>
+        <div className='flex flex-col items-center justify-center w-full h-full border p-4 rounded-md'>
+          <span className='text-text-100'>Size Before:</span>
+          <span className='text-text-150'>0 Bytes</span>
+        </div>
+        <div className='flex flex-col items-center justify-center w-full h-full border p-4 rounded-md'>
+          <span className='text-text-100'>Backup Folder:</span>
+          <span className='text-text-150'>None</span>
+        </div>
+        <div className='flex flex-col items-center justify-center w-full h-full border p-4 rounded-md'>
+          <span className='text-text-100'>Size After:</span>
+          <span className='text-text-150'>0 Bytes</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Report
+export default Report;
