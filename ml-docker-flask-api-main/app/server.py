5bc85b895028c489dde316177292e3f0e8a4be35
Minh Vy ha <minhvy.ha@outlook.com>
2024-06-27 12:27:06 +1000

    refactor: Add .DS_Store to .gitignore

---
+++ ml-docker-flask-api-main/app/server.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+import pickle
+
+# model = joblib.load('app/model.joblib')
+
+with open('app/model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+with open('app/transformer.pkl', 'rb') as file:
+    transformer = pickle.load(file)
+
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    return {'message': 'Iris model API'}
+
+@app.post('/predict')
+def predict(data: dict):
+    text = transformer.transform([data['text']]).toarray()
+    return_data = model.predict(text)
+
+    return {'hate_speech_level': return_data[0]}
+
