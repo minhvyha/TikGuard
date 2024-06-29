b96af81d926744c47a99b4d70edfc3eb58236331
Minh Vy ha <minhvy.ha@outlook.com>
2024-06-29 15:27:54 +1000

    refactor: Update AssemblyAI integration for audio transcription

---
+++ tikguard/app/video/sightengine/api/route.ts
@@ -0,0 +1,22 @@
+
+import { AssemblyAI } from 'assemblyai';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  const data = await request.json();
+
+  const client = new AssemblyAI({
+    apiKey: '8faf7a4d58eb4638a69d44a367f1f8dd',
+  });
+  const audioUrl = data.path;
+  const config = {
+    audio_url: audioUrl,
+    language_detection: true,
+
+
+  };
+
+  const transcript = await client.transcripts.transcribe(config);
+  console.log(transcript.text);
+  return NextResponse.json({ text: transcript.text });
+}
