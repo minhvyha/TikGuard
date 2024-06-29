82da2bbf65b0fc50b346d3e835957cd9c25e1df9
Minh Vy ha <minhvy.ha@outlook.com>
2024-06-30 02:01:47 +1000

    refactor: Optimize Loading component for better performance and readability

--- tikguard/components/Loading.tsx
+++ tikguard/components/Loading.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import * as React from 'react';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
@@ -7,16 +7,12 @@ import { useStore } from '@/app/context/context';
 import Backdrop from '@mui/material/Backdrop';
 
 export default function Loading() {
-  const {loading} = useStore();
+  const { loading } = useStore();
   return (
-<Backdrop
-        sx={{ color: '#fa8e30', zIndex: 1000 }}
-        open={loading}
-      >
-    <Box sx={{ display: 'flex' }}>
-      <CircularProgress />
-    </Box>
-      </Backdrop>
-
+    <Backdrop sx={{ color: '#fa8e30', zIndex: 1000 }} open={loading}>
+      <Box sx={{ display: 'flex' }}>
+        <CircularProgress />
+      </Box>
+    </Backdrop>
   );
 }
