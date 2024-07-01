2267a631beacfaa0635cc9c3d8e01632174da234
Minh Vy ha <minhvy.ha@outlook.com>
2024-07-01 13:19:04 +1000

    refactor: Update SightEngine API integration for text and image safety checks

---
+++ tikguard/components/BasicSelect.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+export default function BasicSelect({list, value, callback} : {list: {
+  value: string,
+    label: string,
+
+}[], value: string, callback: (value: string) => void}) {
+
+  const selectList = list.map((item, index) => {
+    return (
+      <MenuItem key={index} value={item.value}>{item.label}</MenuItem>
+    )
+  }
+  )
+  
+  const handleChange = (event: SelectChangeEvent) => {
+    callback(event.target.value as string);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={value}
+          label="Age"
+          onChange={handleChange}
+        >
+          {selectList}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
