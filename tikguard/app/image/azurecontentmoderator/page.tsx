3a6a9cd96650d348d6dced0236b99e478052bc23
Minh Vy ha <minhvy.ha@outlook.com>
2024-07-02 12:03:49 +1000

    refactor: Update Azure Content Moderator page layout and functionality

--- tikguard/app/image/azurecontentmoderator/page.tsx
+++ tikguard/app/image/azurecontentmoderator/page.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import CircularProgressBasic from '@/components/ImageAnalysePage/CircularProgressBasic'
 import UrlInput from '@/components/ImageAnalysePage/UrlInput'
+import ImageHeader from '@/components/ImageAnalysePage/ImageHeader'
 const page = () => {
   return (
-    <div className='flex flex-row py-5'>
+    <div className='flex flex-col gap-2 py-5 dark:text-white text-black dark:bg-black bg-white'>
+      <ImageHeader page='azure' />
+      <div className='flex flex-row'>
+
       <div className='flex-1'>
         <UrlInput apiRoute='/image/azurecontentmoderator/api' />
       </div>
       <div className='flex-1'>
 
       <CircularProgressBasic />
+      </div>
       </div>
     </div>
   )
