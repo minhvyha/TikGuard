a48144cbcd2bd54cb55df25d932f9b4102ee8465
Minh Vy ha <minhvy.ha@outlook.com>
2024-07-04 11:28:17 +1000

    refactor: Update TextAnalysePage component for better layout and functionality

--- tikguard/components/TextAnalysePage.tsx
+++ tikguard/components/TextAnalysePage.tsx
@@ -1,11 +1,11 @@
-'use client'
-import React, {useEffect} from 'react'
-import MainInput from './TextAnalysePage/MainInput'
-import TextInput from './TextAnalysePage/TextInput'
-import Report from './TextAnalysePage/Report'
-import { useStore } from '@/app/context/context'
-import { usePathname } from 'next/navigation'
-const TextAnalysePage = ({page} : {page: string}) => {
+'use client';
+import React, { useEffect } from 'react';
+import MainInput from './TextAnalysePage/MainInput';
+import TextInput from './TextAnalysePage/TextInput';
+import Report from './TextAnalysePage/Report';
+import { useStore } from '@/app/context/context';
+import { usePathname } from 'next/navigation';
+const TextAnalysePage = ({ page }: { page: string }) => {
   const pathname = usePathname();
 
   const { setData } = useStore();
@@ -16,19 +16,19 @@ const TextAnalysePage = ({page} : {page: string}) => {
   }, [pathname]);
   return (
     <div className="flex flex-row gap-4 p-6 ">
-        <div className="flex-1 flex flex-col gap-4">
-          <div className="flex">
-            <MainInput apiRoute={'text/' + page} />
-          </div>
-          <div className="flex flex-row gap-2">
-            <TextInput />
-          </div>
+      <div className="flex-1 flex flex-col gap-4">
+        <div className="flex">
+          <MainInput apiRoute={'text/' + page} />
         </div>
-        <div className="flex-1">
-          <Report page={page} />
+        <div className="flex flex-row gap-2">
+          <TextInput />
         </div>
       </div>
-  )
-}
+      <div className="flex-1">
+        <Report page={page} />
+      </div>
+    </div>
+  );
+};
 
-export default TextAnalysePage
+export default TextAnalysePage;
