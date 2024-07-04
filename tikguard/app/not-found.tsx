f4afd916ec3f1ba73794b0e15c00ba9003675ae4
Minh Vy ha <minhvy.ha@outlook.com>
2024-07-05 09:36:00 +1000

    refactor: Remove unnecessary CSS class from NotFound component

--- tikguard/app/not-found.tsx
+++ tikguard/app/not-found.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound: React.FC = () => {
   return (
 
-    <div className='fixed h-dvh w-dvw flex flex-col justify-center items-center'>
+    <div className='h-dvh flex flex-col justify-center items-center'>
       <div className='text-2xl'>404 - Page Not Found</div>
       <p>The page you are looking for does not exist.</p>
     </div>
