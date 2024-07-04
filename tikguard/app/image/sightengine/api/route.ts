e9170b498eb0b282803bce918ed9c1ad823fdcce
Minh Vy ha <minhvy.ha@outlook.com>
2024-07-04 13:15:04 +1000

    refactor: Update Vercel SVG file

--- tikguard/app/image/sightengine/api/route.ts
+++ tikguard/app/image/sightengine/api/route.ts
@@ -2,188 +2,134 @@
 import { NextRequest, NextResponse } from 'next/server';
 const axios = require('axios');
 export const dynamic = 'force-dynamic'; // <- add this to force dynamic render
-export const maxDuration = 60; 
+export const maxDuration = 60;
+
+function cleanResult(result: any) {
+  delete result?.request;
+  delete result?.status;
+  delete result?.media;
+  console.log()
+  result.nudity = {
+    none: result.nudity.none,
+    classes: {
+      sexual_activity: result.nudity.sexual_activity,
+      sexual_display: result.nudity.sexual_display,
+      erotica: result.nudity.erotica,
+      very_suggestive: result.nudity.very_suggestive,
+      suggestive: result.nudity.suggestive,
+      mildly_suggestive: result.nudity.mildly_suggestive,
+
+      bikini: result.nudity.suggestive_classes.bikini,
+      cleavage: result.nudity.suggestive_classes.cleavage,
+      cleavage_categories:
+        result.nudity.suggestive_classes.cleavage_categories.none,
+
+      lingerie: result.nudity.suggestive_classes.lingerie,
+      male_chest: result.nudity.suggestive_classes.male_chest,
+      male_chest_categories:
+        result.nudity.suggestive_classes.male_chest_categories.none,
+
+      male_underwear: result.nudity.suggestive_classes.male_underwear,
+      miniskirt: result.nudity.suggestive_classes.miniskirt,
+      minishort: result.nudity.suggestive_classes.minishort,
+      nudity_art: result.nudity.suggestive_classes.nudity_art,
+      schematic: result.nudity.suggestive_classes.schematic,
+      sextoy: result.nudity.suggestive_classes.sextoy,
+      suggestive_focus: result.nudity.suggestive_classes.suggestive_focus,
+      suggestive_pose: result.nudity.suggestive_classes.suggestive_pose,
+      swimwear_male: result.nudity.suggestive_classes.swimwear_male,
+      swimwear_one_piece: result.nudity.suggestive_classes.swimwear_one_piece,
+      visibly_undressed: result.nudity.suggestive_classes.visibly_undressed,
+      other: result.nudity.suggestive_classes.other,
+
+      sea_lake_pool: result.nudity.context.sea_lake_pool,
+      outdoor_other: result.nudity.context.outdoor_other,
+      indoor_other: result.nudity.context.indoor_other,
+    },
+  };
+  result.weapon = {
+    classes: {
+      firearm: result.weapon.classes.firearm,
+      firearm_gesture: result.weapon.classes.firearm_gesture,
+      firearm_toy: result.weapon.classes.firearm_toy,
+      knife: result.weapon.classes.knife,
+      animated: result.weapon.firearm_type.animated,
+      aiming_threat: result.weapon.firearm_action.aiming_threat,
+      aiming_safe: result.weapon.firearm_action.aiming_safe,
+      in_hand_not_aiming: result.weapon.firearm_action.in_hand_not_aiming,
+      worn_not_in_hand: result.weapon.firearm_action.worn_not_in_hand,
+      not_worn: result.weapon.firearm_action.not_worn,
+    },
+  };
+  result['self-harm'] = {
+    prob: result['self-harm'].prob,
+    classes: {
+      real: result['self-harm'].type.real,
+      fake: result['self-harm'].type.fake,
+      animated: result['self-harm'].type.animated,
+    },
+  };
+  result.offensive = {
+    prob: result.offensive.prob,
+    classes: {
+      nazi: result.offensive.nazi,
+      confederate: result.offensive.confederate,
+      supremacist: result.offensive.supremacist,
+      terrorist: result.offensive.terrorist,
+      middle_finger: result.offensive.middle_finger,
+    },
+  };
+  result.gore = {
+    prob: result.gore.prob,
+    classes: {
+      very_bloody: result.gore.classes.very_bloody,
+      slightly_bloody: result.gore.classes.slightly_bloody,
+      body_organ: result.gore.classes.body_organ,
+      serious_injury: result.gore.classes.serious_injury,
+      superficial_injury: result.gore.classes.superficial_injury,
+      corpse: result.gore.classes.corpse,
+      skull: result.gore.classes.skull,
+      unconscious: result.gore.classes.unconscious,
+      body_waste: result.gore.classes.body_waste,
+      other: result.gore.classes.other,
+      animated: result.gore.type.animated,
+      fake: result.gore.type.fake,
+      real: result.gore.type.real,
+    },
+  };
+
+  return result;
+}
 
 export async function GET(request: NextRequest) {
-  // try {
-  //   let url = request.nextUrl.searchParams.get('url') ?? '';
+  try {
+    let url = request.nextUrl.searchParams.get('url') ?? '';
 
-  //   const result = await fetch(
-  //     'https://api.sightengine.com/1.0/check.json?' +
-  //       new URLSearchParams({
-  //         url: url,
-  //         models:
-  //           'nudity-2.1,weapon,alcohol,recreational_drug,offensive,gore-2.0,qr-content,tobacco,violence,self-harm,money,gambling',
-  //         api_user: process.env.SIGHTENGINE_USER ?? '',
-  //         api_secret: process.env.SIGHTENGINE_SECRET ?? '',
-  //       }),
-  //     {
-  //       method: 'GET',
-  //     }
-  //   );
-  //   const data = await result.json();
-
-  //   return NextResponse.json(data);
-  // } catch (error) {
-  //   console.error('Error uploading file:', error);
-  //   return NextResponse.json({ error });
-  // }
-  let result = {
-    "status": "success",
-    "request": {
-        "id": "req_gyziWGpcR4TOYZ3t2yjyU",
-        "timestamp": 1719996773.076914,
-        "operations": 11
-    },
-    "nudity": {
-        "sexual_activity": 0.01,
-        "sexual_display": 0.01,
-        "erotica": 0.01,
-        "very_suggestive": 0.01,
-        "suggestive": 0.01,
-        "mildly_suggestive": 0.03,
-        "suggestive_classes": {
-            "bikini": 0.01,
-            "cleavage": 0.01,
-            "cleavage_categories": {
-                "very_revealing": 0.01,
-                "revealing": 0.01,
-                "none": 0.99
-            },
-            "lingerie": 0.01,
-            "male_chest": 0.01,
-            "male_chest_categories": {
-                "very_revealing": 0.01,
-                "revealing": 0.01,
-                "slightly_revealing": 0.01,
-                "none": 0.99
-            },
-            "male_underwear": 0.01,
-            "miniskirt": 0.01,
-            "minishort": 0.01,
-            "nudity_art": 0.01,
-            "schematic": 0.01,
-            "sextoy": 0.01,
-            "suggestive_focus": 0.01,
-            "suggestive_pose": 0.01,
-            "swimwear_male": 0.01,
-            "swimwear_one_piece": 0.01,
-            "visibly_undressed": 0.01,
-            "other": 0.01
-        },
-        "none": 0.97,
-        "context": {
-            "sea_lake_pool": 0.03,
-            "outdoor_other": 0.81,
-            "indoor_other": 0.16
-        }
-    },
-    "weapon": {
-        "classes": {
-            "firearm": 0.01,
-            "firearm_gesture": 0.01,
-            "firearm_toy": 0.01,
-            "knife": 0.01
-        },
-        "firearm_type": {
-            "animated": 0.01
-        },
-        "firearm_action": {
-            "aiming_threat": 0.01,
-            "aiming_safe": 0.01,
-            "in_hand_not_aiming": 0.01,
-            "worn_not_in_hand": 0.01,
-            "not_worn": 0.01
-        }
-    },
-    "recreational_drug": {
-        "prob": 0.01,
-        "classes": {
-            "cannabis": 0.01,
-            "cannabis_logo_only": 0.01,
-            "cannabis_plant": 0.01,
-            "cannabis_drug": 0.01,
-            "recreational_drugs_not_cannabis": 0.01
-        }
-    },
-    "medical": {
-        "prob": 0.01,
-        "classes": {
-            "pills": 0.01,
-            "paraphernalia": 0.01
-        }
-    },
-    "alcohol": {
-        "prob": 0.01
-    },
-    "offensive": {
-        "prob": 0.01,
-        "nazi": 0.01,
-        "confederate": 0.01,
-        "supremacist": 0.01,
-        "terrorist": 0.01,
-        "middle_finger": 0.01
-    },
-    "gore": {
-        "prob": 0.01,
-        "classes": {
-            "very_bloody": 0.01,
-            "slightly_bloody": 0.01,
-            "body_organ": 0.01,
-            "serious_injury": 0.01,
-            "superficial_injury": 0.01,
-            "corpse": 0.01,
-            "skull": 0.01,
-            "unconscious": 0.01,
-            "body_waste": 0.01,
-            "other": 0.01
-        },
-        "type": {
-            "animated": 0.01,
-            "fake": 0.01,
-            "real": 0.01
-        }
-    },
-    "tobacco": {
-        "prob": 0.01,
-        "classes": {
-            "regular_tobacco": 0.01,
-            "ambiguous_tobacco": 0.01
-        }
-    },
-    "violence": {
-        "prob": 0.01,
-        "classes": {
-            "physical_violence": 0.01,
-            "firearm_threat": 0.01,
-            "combat_sport": 0.01
-        }
-    },
-    "self-harm": {
-        "prob": 0.01,
-        "type": {
-            "real": 0.01,
-            "fake": 0.01,
-            "animated": 0.01
-        }
-    },
-    "money": {
-        "prob": 0.01
-    },
-    "gambling": {
-        "prob": 0.01
-    },
-    "media": {
-        "id": "med_gyziMYNSrxx9DWTKyAEPN",
-        "uri": "https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI"
-    }
-}
-delete result?.request;
-delete result?.status;
-delete result?.media
-
-
-return NextResponse.json(result);
+    const result = await fetch(
+    'https://api.sightengine.com/1.0/check.json?' +
+        new URLSearchParams({
+          url: url,
+          models:
+            'nudity-2.1,weapon,alcohol,recreational_drug,offensive,gore-2.0,qr-content,tobacco,violence,self-harm,money,gambling',
+          api_user: process.env.SIGHTENGINE_USER ?? '',
+          api_secret: process.env.SIGHTENGINE_SECRET ?? '',
+        }),
+      {
+        method: 'GET',
+      }
+    );
+    let data = await result.json();
+    console.log(data)
+    delete data?.request;
+    delete data?.status;
+    delete data?.media;
+    data = cleanResult(data);
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    return NextResponse.json({ error });
+  }
+  
 }
 
 export async function POST(request: Request) {
