d9fa8d2792dc71c02e8bf7f96932dd2b802fc01d
Minh Vy ha <minhvy.ha@outlook.com>
2024-07-04 20:02:53 +1000

    refactor: Add videoApiPage to constant/index.ts

---
+++ tikguard/constant/api/video/index.ts
@@ -0,0 +1,12 @@
+let videoApiPage = [
+  {
+    name: 'Azure Content Moderator',
+    url: 'https://learn.microsoft.com/en-us/azure/ai-services/content-moderator/overview',
+    path: 'image/azurecontentmoderator',
+    description:
+      'Azure Content Moderator is an AI service for detecting and flagging offensive or undesirable content',
+    category: 'AI Text Analysis',
+    },
+];
+
+export {videoApiPage}
--- tikguard/constant/index.ts
+++ tikguard/constant/index.ts
@@ -1,4 +1,16 @@
-import { textApiPage, defaultValue, header, languageCode } from "./api/text";
-import {imageApiPage, imageHeader, defaultImageApi} from "./api/image";
-import { aiApiPage } from "./api/ai";
-export { textApiPage, defaultValue, header,imageHeader, defaultImageApi, imageApiPage, aiApiPage, languageCode };
+import { textApiPage, defaultValue, header, languageCode } from './api/text';
+import { imageApiPage, imageHeader, defaultImageApi } from './api/image';
+import { aiApiPage } from './api/ai';
+import { videoApiPage } from './api/video';
+
+export {
+  textApiPage,
+  videoApiPage,
+  defaultValue,
+  header,
+  imageHeader,
+  defaultImageApi,
+  imageApiPage,
+  aiApiPage,
+  languageCode,
+};
