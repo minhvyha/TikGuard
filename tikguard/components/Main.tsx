b53ee302c3b37ad1628cc58fdb9fef4fa0b517ca
Minh Vy ha <minhvy.ha@outlook.com>
2024-07-06 10:54:55 +1000

    refactor: Update Main component layout and add Model component

--- tikguard/components/Main.tsx
+++ tikguard/components/Main.tsx
@@ -4,14 +4,15 @@ import React  from 'react'
 import { textApiPage, imageApiPage, aiApiPage, videoApiPage } from '@/constant'
 import DemoContainer from './DemoContainer'
 import Contact from './Contact'
+import Model from './Model'
 const Main = () => {
 
   return (
-    <div className='flex justify-center flex-col items-center w-full py-8 gap-12 pb-24'>
+    <div className='flex justify-center flex-col items-center w-full py-8 pt-20 gap-12 pb-24'>
+      <Model />
       <DemoContainer id='text' api={textApiPage} title='Text Analysis API' subtitle='Detect and Prevent Hate Speech Effectively' />
       <DemoContainer id='image' api={imageApiPage} title='Image Analysis API' subtitle='Identify and Block Hateful Visual Content' />
       <DemoContainer id='video' api={videoApiPage} title='Video Analysis API' subtitle='Analyze and Filter Hateful Video Content' />
-      <DemoContainer id='ml' api={aiApiPage} title='Our Machine Learning Model' subtitle='Prevent Hate Speech with Precision with'/>
       <Contact />
     </div>
   )
---
+++ tikguard/components/Model.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Model = () => {
+  return (
+    <div>
+      <div>
+        <h1>TikGuard: Detect Hate Speech & Violent Content</h1>
+        <h3>
+          TikGuard leverages advanced machine learning algorithms to provide
+          robust detection of hate speech, hateful imagery, and violent content.
+          Upload text, images, or videos to quickly and accurately identify
+          harmful material, ensuring a safer online environment.
+        </h3>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Model;
