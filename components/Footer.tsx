e7156e09deef866d14bad762f153c9ef56584418
Minh Vy ha <minhvy.ha@outlook.com>
2024-07-11 18:03:12 +1000

    docs: Update Footer component with revised text and styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,11 +16,11 @@ export function Footer() {
               className="h-12 w-12"
               alt="Flowbite Logo"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-gray-600">
               TikGuard
             </span>
           </a>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-600 sm:mb-0 dark:text-gray-400">
             <li>
               <a href="/#text" className="hover:underline me-4 md:me-6">
                 Text API
@@ -49,7 +49,7 @@ export function Footer() {
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <span className="block text-sm text-gray-600 sm:text-center dark:text-gray-400">
           © 2024{' '}
           <a href="https://tikguard.vercel.app/" className="hover:underline">
             TikGuard
