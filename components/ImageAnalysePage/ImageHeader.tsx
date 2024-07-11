baecaaf681c61e97e86ac6cadad92cf65e06d4f4
Minh Vy ha <minhvy.ha@outlook.com>
2024-07-11 18:05:16 +1000

    chore: Update ImageHeader component with dark mode support

--- components/ImageAnalysePage/ImageHeader.tsx
+++ components/ImageAnalysePage/ImageHeader.tsx
@@ -4,7 +4,7 @@ import { imageHeader } from '@/constant'
 
 const Header = ({page} : {page: string}) => {
   return (
-    <div className='flex flex-col justify-center items-center'>
+    <div className='flex text-gray-800 dark:text-white flex-col justify-center items-center'>
         <h1 className='text-2xl'>{imageHeader[page as keyof typeof imageHeader].title}</h1>
         <h2 className='text-text-150'>{imageHeader[page as keyof typeof imageHeader].description}</h2>
       </div>
