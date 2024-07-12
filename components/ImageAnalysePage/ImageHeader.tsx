8ba74b4cbd60b189a84b346ca0415972144859d4
Minh Vy ha <minhvy.ha@outlook.com>
2024-07-12 11:11:51 +1000

    refactor: Update ImageHeader component with consistent formatting and TypeScript types

--- components/ImageAnalysePage/ImageHeader.tsx
+++ components/ImageAnalysePage/ImageHeader.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import { imageHeader } from '@/constant'
+import React from 'react';
+import { imageHeader } from '@/constant';
 
-
-const Header = ({page} : {page: string}) => {
+const Header = ({ page }: { page: string }) => {
   return (
-    <div className='flex text-gray-800 dark:text-white flex-col justify-center items-center'>
-        <h1 className='text-2xl'>{imageHeader[page as keyof typeof imageHeader].title}</h1>
-        <h2 className='text-text-150'>{imageHeader[page as keyof typeof imageHeader].description}</h2>
-      </div>
-  )
-}
+    <div className="flex text-gray-800 dark:text-white flex-col justify-center items-center">
+      <h1 className="text-2xl">
+        {imageHeader[page as keyof typeof imageHeader].title}
+      </h1>
+      <h2 className="text-text-150">
+        {imageHeader[page as keyof typeof imageHeader].description}
+      </h2>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
